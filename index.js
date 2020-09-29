409ee281ae98a79e194bbebffea79debc425952f
dherendra dev <dheerudev101@gmail.com>
2020-09-29 20:08:42 +0530

    changes side

--- index.js
+++ index.js
@@ -18,22 +18,22 @@ mongoose.connect(`${process.env.MONGODB_URL}/expense_tracker_app?retryWrites=tru
   console.log('DataBase Connected!!')
 });
 
+app.use(cors({origin:"https://expense-tracker-app-with-mern.herokuapp.com"}));
 app.options('*', cors())
-app.use(cors({origin:"https://expense-tracker-app-with-mern.herokuapp.com/"}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }))
 
-// app.use((req, res, next)=>{
-//     res.header("Access-Control-Allow-Origin","*");
-//     res.header("Access-Control-Allow-Credentials" ,'true');
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.setHeader('Access-Control-Allow-Headers','GET, POST, PATCH, PUT, DELETE, OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
-//     if(req.method ==='OPTIONS'){
-//             return res.sendStatus(200);
-//         }
-//     next()
-// });
+app.use((req, res, next)=>{
+    res.header("Access-Control-Allow-Origin","*");
+    res.header("Access-Control-Allow-Credentials" ,'true');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Headers','GET, POST, PATCH, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers','Content-Type,Authorization');
+    if(req.method ==='OPTIONS'){
+            return res.sendStatus(200);
+        }
+    next()
+});
 
 
 app.use(serveStatic(__dirname + "/build")); //
